7f0aa805fdbcc858deffceb5ebc30fc13feacf3b
aditya23409876543 <dhanrajaditya230@gmail.com>
2025-03-23 03:26:16 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,30 @@ import MarketPlace from './Components/MarketPlace'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './Components/ui/connectBUtton';
 import LandingPage from './Components/LandingPage';
+import Authentication from './Components/Authentication'
+import HomePage from './Components/HomePage';
 
 
 const App = () => {
   return (
-      <>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LandingPage/>}/>
-            <Route path="/marketplace" element={<MarketPlace/>}/>
-          </Routes>
-        </BrowserRouter>
-      </>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/marketplace" element={<MarketPlace/>}/>
+      </Routes>
+    </BrowserRouter>
+  </>
+    
+    
+      // <>
+      //   <BrowserRouter>
+      //     <Routes>
+      //       <Route path="/" element={<LandingPage/>}/>
+      //       <Route path="/marketplace" element={<MarketPlace/>}/>
+      //     </Routes>
+      //   </BrowserRouter>
+      // </>
   
   )
 }
--- src/Components/authentication.jsx
+++ src/Components/authentication.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 
-export default function AUthenticate() {
+export default function Authentication() {
 
     const account = useActiveAccount();
     const navigate = useNavigate();
